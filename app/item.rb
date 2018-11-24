e1d9768180ced01586409e39be1f28c925d92bfe
Zachary Lehmann <zlehmann@gmail.com>
2018-11-24 23:30:12 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -1,15 +1,8 @@
 class Item
   attr_accessor :name, :price
 
-  @@item = []
-
   def initialize(name,price)
     @name = name
     @price = price
-    @@item << self
-  end
-
-  def self.items
-    @@item
   end
 end
